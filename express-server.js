cf217eef60b796c19b83368ca46db4a9791c213a
Isaac Vallee <isaacvallee@gmail.com>
2017-09-13 00:09:17 +0000

    make post add new keyvalue pair to database

--- express-server.js
+++ express-server.js
@@ -2,6 +2,8 @@
 // load the things we need
 const express = require('express');
 const app = express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 // database
 const urlDatabase = {
@@ -25,6 +27,10 @@ app.get('/urls', (req, res) => {
     res.render('urls_index', templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 // single url in shortened form
 app.get("/urls/:id", (req, res) => {
   let templateVars = { shortURL: req.params.id,
@@ -32,5 +38,19 @@ app.get("/urls/:id", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // debug statement to see POST parameters
+  const randomStr = (generateRandomString());
+  urlDatabase[randomStr] = req.body.longURL;
+  console.log(urlDatabase);
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
 app.listen(8080);
 console.log('8080 is the magic port');
+
+function generateRandomString() {
+  const randomStr = Math.floor((1 + Math.random()) * 0x100000).toString(16);
+  return randomStr;
+}
